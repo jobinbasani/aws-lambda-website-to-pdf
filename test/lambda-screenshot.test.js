6f4389542289e2f3cc0b6478aa49eb10f4af9b95
Jobin Basani <jobinbasani@gmail.com>
2022-05-01 10:18:58 -0300

    Initial commit

---
+++ test/lambda-screenshot.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const LambdaScreenshot = require('../lib/lambda-screenshot-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-screenshot-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new LambdaScreenshot.LambdaScreenshotStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
