1fdbe0437aa0ec592e6bb867acdd795321e1a1e2
Jobin Basani <jobinbasani@gmail.com>
2022-05-01 13:44:46 -0300

    Initial commit

---
+++ functions/url-screenshot-function.js
@@ -0,0 +1,34 @@
+const chromium = require('chrome-aws-lambda');
+
+exports.handler = async (event)=>{
+
+    const url = event.queryStringParameters.url;
+
+    const browser = await chromium.puppeteer.launch({
+        args: chromium.args,
+        headless: true,
+        ignoreHTTPSErrors: true,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+    });
+
+    const page = await browser.newPage();
+    await page.goto(url, {waitUntil: 'networkidle0'});
+
+    const buffer = await page.screenshot({
+        type: 'png',
+        fullPage: true,
+        omitBackground: true,
+        encoding: 'binary',
+    })
+
+    const response = {
+        statusCode: 200,
+        headers:{
+            "Content-type":"image/png",
+        },
+        body: buffer,
+    };
+
+    return response;
+}
